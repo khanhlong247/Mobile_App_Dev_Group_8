47854badd69821a283c3f44b562872ee86927269
maituyet04 <luutuyet.b.d.h@gmail.com>
2024-12-12 08:51:44 +0000

    Luu Mai Tuyet commit 12/12/2024

--- test1/t1/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ test1/t1/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/workspaces/Nhom-8/test1/flutter"
+export "FLUTTER_ROOT=/workspaces/Nhom-8/Group8Project/flutter"
 export "FLUTTER_APPLICATION_PATH=/workspaces/Nhom-8/test1/t1"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
