ed6886169171a0c84aa29085054e23db5d360769
maituyet04 <luutuyet.b.d.h@gmail.com>
2025-01-06 07:28:40 +0000

    Lưu Mai Tuyết commit 06/01/2025

---
+++ Group8Project/luumaituyet_project/lib/user_object.dart
@@ -0,0 +1,10 @@
+import 'user_class.dart';
+
+// Danh sách các bản ghi người dùng (giả định)
+final List<User> userList = [
+  User(username: 'Lưu Mai Tuyết', password: 'pass1', role: 'admin'),
+  User(username: 'Trương Long Khánh', password: 'pass2', role: 'editor'),
+  User(username: 'Nguyễn Văn A', password: 'pass3', role: 'viewer'),
+  User(username: 'Nguyễn Văn B', password: 'pass4', role: 'guest'),
+  User(username: 'Nguyễn Văn C', password: 'pass5', role: 'user'),
+];
